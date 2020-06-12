034e0cbdded04cd8bf8b710be07d98222b9ade4c
kreuzerk <kevin.kreuzer90@icloud.com>
2020-06-12 10:43:32 +0200

    chore(testfiles): add further test files

---
+++ test/sampleDir/baz/baz.ts
@@ -0,0 +1,10 @@
+import {foo} from '../foo/foo';
+import {bar} from '../bar/bar';
+
+import {Observable} from 'rxjs';
+import {Blub} from '@custom/something';
+
+export const baz = 'baz';
+console.log('Foo', foo);
+
+console.log()
---
+++ test/sampleDir/baz/qux/qux.ts
@@ -0,0 +1,6 @@
+import {enableProdMode} from '@angular/core';
+import {baz} from '../baz';
+
+import {foo} from '../../foo/foo';
+
+console.log('Foo', foo);
