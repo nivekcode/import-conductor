103959a95a66844cbb7b981acf5b96fe411e678a
kreuzerk <kevin.kreuzer90@icloud.com>
2020-06-13 15:31:43 +0200

    chore(traverse): extract traverse

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -11,11 +11,20 @@ interface ImportCategories {
     sameModulePot: Map<string, string>;
 }
 
+interface ImportInformation {
+    importStatementMap: Map<string, string>,
+    startPosition: number;
+    endPosition: number;
+}
+
 async function* walk(dir: string): any {
     for await (const d of await promises.opendir(dir)) {
         const entry = join(dir, d.name);
-        if (d.isDirectory()) yield* await walk(entry);
-        else if (d.isFile()) yield entry;
+        if (d.isDirectory()) {
+            yield* await walk(entry);
+        } else if (d.isFile()) {
+            yield entry;
+        }
     }
 }
 
@@ -26,7 +35,6 @@ function categorizeImportLiterals(importLiterals: Map<string, string>): ImportCa
         differentUserModulePot: new Map<string, string>(),
         sameModulePot: new Map<string, string>()
     }
-
     importLiterals.forEach((fullImportStatement: string, importLiteral: string) => {
         if (importLiteral.startsWith(`'./`)) {
             pots.sameModulePot.set(importLiteral, fullImportStatement);
@@ -44,11 +52,15 @@ function categorizeImportLiterals(importLiterals: Map<string, string>): ImportCa
         }
         pots.thirdPartyImportPot.set(importLiteral, fullImportStatement);
     });
+    return pots;
+}
+
+function sortImportCategories(importCategories: ImportCategories): ImportCategories {
     return {
-        thirdPartyImportPot: new Map([...pots.thirdPartyImportPot].sort()),
-        userLibraryPot: new Map([...pots.userLibraryPot].sort()),
-        differentUserModulePot: new Map([...pots.differentUserModulePot].sort()),
-        sameModulePot: new Map([...pots.sameModulePot].sort()),
+        thirdPartyImportPot: new Map([...importCategories.thirdPartyImportPot].sort()),
+        userLibraryPot: new Map([...importCategories.userLibraryPot].sort()),
+        differentUserModulePot: new Map([...importCategories.differentUserModulePot].sort()),
+        sameModulePot: new Map([...importCategories.sameModulePot].sort()),
     };
 }
 
@@ -71,7 +83,7 @@ function formatImportStatements(sortedPots: ImportCategories) {
             result += '\n\n';
         }
         [...sortedPot.values()].forEach((fullImportLiteral: string, index: number) =>
-            result += index === sortedPot.size - 1 ? `${fullImportLiteral}`: `${fullImportLiteral}\n`);
+            result += index === sortedPot.size - 1 ? `${fullImportLiteral}` : `${fullImportLiteral}\n`);
     }
 
     updateResult(sortedPots.thirdPartyImportPot, false);
@@ -94,40 +106,46 @@ function collectImportStatement(importSegments: any) {
         }, '');
 }
 
+function getImportInformation(rootNode: ts.Node): ImportInformation {
+    const importNodes: ts.Node[] = [];
+    const importStatementMap = new Map<string, string>();
+
+    const traverse = (node: ts.Node) => {
+        if (ts.isImportDeclaration(node)) {
+            const importSegments = node.getChildren();
+            const completeImportStatement = collectImportStatement(importSegments);
+            const importLiteral = importSegments.find(
+                segment => segment.kind === ts.SyntaxKind.StringLiteral
+            )?.getText();
+            if (importLiteral) {
+                importStatementMap.set(importLiteral, completeImportStatement);
+            }
+            importNodes.push(node);
+        }
+    }
+    rootNode.forEachChild(traverse);
+    return {importStatementMap, startPosition: importNodes[0]?.pos, endPosition: importNodes[importNodes?.length - 1]?.end}
+}
+
 (async () => {
     for await (const p of walk(resolve(__dirname, '../../test'))) {
         const fileContent = await promises.readFile(p);
         const rootNode = ts.createSourceFile(p, fileContent.toString(), ts.ScriptTarget.Latest, true);
+        const importInformation = getImportInformation(rootNode);
 
-        const importNodes: ts.Node[] = [];
-        const importStatementMap = new Map<string, string>();
-
-        const traverse = (node: ts.Node) => {
-            if (ts.isImportDeclaration(node)) {
-                const importSegments = node.getChildren();
-                const completeImportStatement = collectImportStatement(importSegments);
-                const importLiteral = importSegments.find(
-                    segment => segment.kind === ts.SyntaxKind.StringLiteral
-                )?.getText();
-                if (importLiteral) {
-                    importStatementMap.set(importLiteral, completeImportStatement);
-                }
-                importNodes.push(node);
-            }
-        }
-        rootNode.forEachChild(traverse);
-
-        if (importStatementMap.size > 0) {
-            const categorizedImports = categorizeImportLiterals(importStatementMap);
-            let result = formatImportStatements(categorizedImports);
+        if (importInformation.importStatementMap.size > 0) {
+            const categorizedImports = categorizeImportLiterals(importInformation.importStatementMap);
+            const sortedAndCategorizedImports = sortImportCategories(categorizedImports);
+            let result = formatImportStatements(sortedAndCategorizedImports);
 
             const updatedContent =
-                fileContent.slice(0, importNodes[0].pos) +
+                fileContent.slice(0, importInformation.startPosition) +
                 result +
-                fileContent.slice(importNodes[importNodes.length - 1].end);
+                fileContent.slice(importInformation.endPosition);
 
             if (updatedContent !== fileContent.toString()) {
                 // await promises.writeFile(p, updatedContent);
+                console.log('New content', updatedContent);
             }
             console.log('Done');
         }
