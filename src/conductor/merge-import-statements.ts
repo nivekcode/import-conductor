661d594cf2f28715b962a96160e94baf8da74387
shaharkazaz <shahar.kazaz@gmail.com>
2020-08-31 00:25:14 +0300

    fix: 🐛 merge statements breaks with trailing comma

--- src/conductor/merge-import-statements.ts
+++ src/conductor/merge-import-statements.ts
@@ -1,4 +1,7 @@
 export function mergeImportStatements(importStatementOne, importStatementTwo): string {
-  const importedValues = importStatementTwo.replace(/\n\s*/g, '').match('{(.*)}')[1];
-  return importStatementOne.replace('}', `,${importedValues}}`);
+  let importedValues = importStatementTwo.replace(/\n\s*/g, '').match('{(.*)}')[1];
+  const hasTrailingComma = /,\s*}/.test(importStatementOne);
+  importedValues = hasTrailingComma ? `${importedValues},}` : `,${importedValues}}`;
+
+  return importStatementOne.replace('}', importedValues);
 }
