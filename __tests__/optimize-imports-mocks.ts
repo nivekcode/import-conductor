4bd9aa4867f06a61187756d2a6ddf44187a13a35
shaharkazaz <shahar.kazaz@gmail.com>
2020-09-01 16:45:52 +0300

    test: 💍 update tests

---
+++ __tests__/optimize-imports-mocks.ts
@@ -0,0 +1,57 @@
+export type testCase = { input: string; expected: string };
+
+export const readmeExample: testCase = {
+  input: `import fs from 'fs';
+import { CustomerService } from './customer.service';
+import { Customer } from './customer.model';
+import { Order } from '../order/order.model';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from '@myorg/logger';
+import { Observable } from 'rxjs';
+import { spawn } from 'child_process';`,
+  expected: `import { Component, OnInit } from '@angular/core';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import { Observable } from 'rxjs';
+
+import { LoggerService } from '@myorg/logger';
+
+import { Order } from '../order/order.model';
+
+import { Customer } from './customer.model';
+import { CustomerService } from './customer.service';`,
+};
+
+export const comments: testCase = {
+  input: `// file level comments shouldn't move
+import fs from 'fs';
+import { CustomerService } from './customer.service';
+import { Customer } from './customer.model';
+// should be above this import
+import { Order } from '../order/order.model';
+import { Component, OnInit } from '@angular/core';
+/* I will follow LoggerService wherever he goes */
+import { LoggerService } from '@myorg/logger';
+/**
+* important comment about Observables  
+*/
+import { Observable } from 'rxjs';
+import { spawn } from 'child_process';`,
+  expected: `// file level comments shouldn't move
+import { Component, OnInit } from '@angular/core';
+import { spawn } from 'child_process';
+import fs from 'fs';
+/**
+* important comment about Observables  
+*/
+import { Observable } from 'rxjs';
+
+/* I will follow LoggerService wherever he goes */
+import { LoggerService } from '@myorg/logger';
+
+// should be above this import
+import { Order } from '../order/order.model';
+
+import { Customer } from './customer.model';
+import { CustomerService } from './customer.service';`,
+};
--- playground/sampleDir/main.ts
+++ playground/sampleDir/main.ts
@@ -1,16 +1,16 @@
-import { sync } from 'glob';
-import { isVariableDeclaration } from 'typescript';
+// file level comments shouldn't move
+import { spawn } from 'child_process';
+import fs from 'fs';
+/**
+ * important comment about Observables
+ */
+import { Observable } from 'rxjs';
 
-import { FooModule } from '../someModule';
-import { enableProdMode } from '@custom/something';
+// should be above this import
+import { Order } from '../order/order.model';
+import { Component, OnInit } from '@angular/core';
+/* I will follow LoggerService wherever he goes */
+import { LoggerService } from '@myorg/logger';
 
-import { environment } from './environments/environment';
-import { SomeModule } from './someModule';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic()
-  .bootstrapModule(SomeModule)
-  .catch((err) => console.error(err));
+import { Customer } from './customer.model';
+import { CustomerService } from './customer.service';
