840de0f54fbc929c985b395084c132031f231db1
shaharkazaz <shahar.kazaz@gmail.com>
2020-09-01 16:47:27 +0300

    docs ✏️: update playground example

--- playground/sampleDir/main.ts
+++ playground/sampleDir/main.ts
@@ -1,16 +1,22 @@
 // file level comments shouldn't move
-import { spawn } from 'child_process';
 import fs from 'fs';
-/**
- * important comment about Observables
- */
-import { Observable } from 'rxjs';
-
+import { CustomerService } from './customer.service';
+import { Customer } from './customer.model';
 // should be above this import
 import { Order } from '../order/order.model';
 import { Component, OnInit } from '@angular/core';
 /* I will follow LoggerService wherever he goes */
 import { LoggerService } from '@myorg/logger';
+/**
+ * important comment about Observables
+ */
+import { Observable } from 'rxjs';
+import { spawn } from 'child_process';
 
-import { Customer } from './customer.model';
-import { CustomerService } from './customer.service';
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(SomeModule)
+  .catch((err) => console.error(err));
